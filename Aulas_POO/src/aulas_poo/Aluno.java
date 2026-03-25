937183b62fcfb9385636cfcb339e98f5a4c42448
AthenaTI22 <amaralgeo22@gmail.com>
2026-03-25 09:48:42 -0300

    Atividade 25/03/2026
    Aprendendo a usar encapsulamento, getters/setters, loop com interação, e herança.

---
+++ Aulas_POO/src/aulas_poo/Aluno.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulas_poo;
+
+/**
+ *
+ * @author 86243542505
+ */
+public class Aluno extends SerHumano{
+    private int matricula;
+    private String curso;
+    private String turma;
+    private String turno;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+    
+}
---
+++ Aulas_POO/src/aulas_poo/JavaMundo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulas_poo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 86243542505
+ */
+public class JavaMundo {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String arm="";
+        Aluno Caio = new Aluno();
+        String res;
+        
+        do{
+            Caio.setNome(JOptionPane.showInputDialog(null,"Digite o nome:","Nome",1));
+            Caio.setIdade(Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a idade:","Idade",1)));
+            Caio.setCorDosOlhos(JOptionPane.showInputDialog(null,"Digite a cor dos olhos:","CorDosOlhos",1));
+            Caio.setCorDosCabelos(JOptionPane.showInputDialog(null,"Digite a cor dos cabelos:","CorDosCabelos",1));
+            Caio.setAltura(Float.parseFloat(JOptionPane.showInputDialog(null,"Digite a altura:","Altura",1)));
+            Caio.setEtnia(JOptionPane.showInputDialog(null,"Digite a etnia:","Etnia",1));
+            
+            
+        arm += "Nome: " + Caio.getNome() + "\n" +
+        "Idade: " + Caio.getIdade() + "\n" +
+        "Cor dos olhos: " + Caio.getCorDosOlhos() + "\n" +
+        "Cor dos cabelos: " + Caio.getCorDosCabelos() + "\n" +
+        "Altura: " + Caio.getAltura() + "\n" +
+        "Etnia: " + Caio.getEtnia() + "\n\n";
+            
+            res=JOptionPane.showInputDialog(null,"Deseja continuar cadastrando [s/n]:","Aviso",0);
+        }while((res.equals("s"))||(res.equals("S")));
+        
+        Caio.falar();
+        Caio.dormir();
+        
+        JOptionPane.showMessageDialog(null,arm,"Relatório", 1);
+       
+    }
+}
